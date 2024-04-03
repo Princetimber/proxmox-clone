78b1a942e51340c39a214c606a7783c91ec93318
Olamide Olaleye <Olamide.Olaleye@outlook.com>
2024-04-03 14:45:35 +0100

    initial: new commit.

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "local_vm_configs" {
+  default = [
+    {
+      name        = "prod-ca1"
+      target_node = "pve"
+      vmid        = 10035
+      cores       = 2
+      sockets     = 1
+      memory      = 4096
+      balloon     = 0
+      full_clone  = true
+      clone       = "prod-winserver-2022-gui"
+      tags        = "windows,prod,ca1"
+      onboot      = false
+      oncreate    = false
+      bios        = "ovmf"
+      bootdisk    = "scsi0"
+      scsihw      = "virtio-scsi-pci"
+      numa        = true
+    },
+    {
+      name        = "prod-s1"
+      target_node = "pve"
+      vmid        = 10036
+      cores       = 2
+      sockets     = 1
+      memory      = 4096
+      balloon     = 0
+      full_clone  = true
+      clone       = "prod-winserver-2022-core"
+      tags        = "windows,prod,s1"
+      onboot      = false
+      oncreate    = false
+      bios        = "ovmf"
+      bootdisk    = "scsi0"
+      scsihw      = "virtio-scsi-pci"
+      numa        = true
+    }
+  ]
+
+}
+
